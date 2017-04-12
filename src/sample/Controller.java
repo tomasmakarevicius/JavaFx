233d53d93f642054cb8659c6c2676efd2114b60d
tomasmakarevicius <tmakarevicius@gmail.com>
2017-04-12 20:18:21 +0300

    Pirma uzduotis

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,15 +3,19 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 
 public class Controller {
 
     @FXML
     private Label text;
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextField textField1;
 
     public void sayHi (ActionEvent event){
-        String prefix = text.getText();
-        text.setText(prefix + " Tomas");
+        text.setText((textField.getText()) + " "+ textField1.getText() );
     }
 }
